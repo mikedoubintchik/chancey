05d9f1269d752503d05770b3be9ed80a2f611105
Moshe Moryosef <moshemoryosef@Moshes-MBP.attlocal.net>
2022-09-24 12:44:05 -0400

    ball component

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,6 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonAvatar } from '@ionic/react';
+import Ball from 'components/ball/Ball';
+
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -16,7 +17,7 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <Ball num={32}></Ball>
       </IonContent>
     </IonPage>
   );
