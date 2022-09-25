d2648b4eb0cc30bcfd512334af0282a92078ca46
Moshe Moryosef <moshemoryosef@Moshes-MBP.attlocal.net>
2022-09-25 18:32:58 -0400

    added charts js

--- src/pages/stats/StatsPage.tsx
+++ src/pages/stats/StatsPage.tsx
@@ -1,13 +1,59 @@
-import { IonCard, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonContent, IonHeader, IonItem, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 
 import LotteryDraw from 'components/lottery-draw/LotteryDraw';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 // import './Tab1.css';
+export const options = {
+  indexAxis: 'y' as const,
+  elements: {
+    bar: {
+      borderWidth: 2,
+    },
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'right' as const,
+    },
+    title: {
+      display: true,
+      text: 'Some stats and shit',
+    },
+  },
+};
 
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => 200),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => 200),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 const StatsPage: React.FC = () => {
   return (
     <IonPage>
-      <IonContent fullscreen></IonContent>
+      <IonContent fullscreen>
+        <IonList>
+          <IonItem>
+            <Bar data={data} options={options} />
+          </IonItem>
+        </IonList>
+      </IonContent>
     </IonPage>
   );
 };
