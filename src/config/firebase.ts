55e4b9804b8e1fb915c5e47bb079cef7d5ce5ddc
Mike Doubintchik <mike.doubintchik@gmail.com>
2022-09-26 23:27:30 -0400

    added app check

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -5,8 +5,19 @@ import { connectFunctionsEmulator, getFunctions } from 'firebase/functions';
 import { getStorage } from 'firebase/storage';
 
 import config from './config';
+const { initializeAppCheck, ReCaptchaV3Provider } = require('firebase/app-check');
 
 const app = initializeApp(config.firebase);
+
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(process.env.REACT_APP_RECAPTCHA_SITE_KEY),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: false,
+});
 
 export const auth = getAuth(app);
 export const storage = getStorage(app);
@@ -16,5 +27,9 @@ export default app;
 
 if (process.env.REACT_APP_ENV === 'development') {
   console.log('development environment');
+
+  // use debug token for app check
+  (window as any).FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+
   connectFunctionsEmulator(functions, 'localhost', 5001);
 }
