3bc8d576dba0baeb3ca914b11647c88072ad2e2a
Mike Doubintchik <mike.doubintchik@gmail.com>
2022-09-26 23:26:57 -0400

    updated function to fetch and return image text

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,32 +4,63 @@ const vision = require('@google-cloud/vision');
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest(async (request, response) => {
-  functions.logger.info('meow', request.body);
-  // functions.logger.info('Hello logs!', { structuredData: true });
+export const helloWorld = functions.https // }) //   // Check tokens continue to your code. //   allowInvalidAppCheckToken: true, // Opt-out: Requests with invalid App // .runWith({
+  .onCall(async (data, context) => {
+    const { fileName } = data;
+    console.log('🚀 ~ file: index.ts ~ line 9 ~ helloWorld ~ fileName', fileName);
 
-  // if (request.method !== 'POST') {
-  //   return response.status(401).json({
-  //     message: 'Not allowed',
-  //   });
-  // }
-  const client = new vision.ImageAnnotatorClient({
-    keyFilename: '../credentials/google_credentials.json',
+    // context.app will be undefined if the request doesn't include an
+    // App Check token. (If the request includes an invalid App Check
+    // token, the request will be rejected with HTTP error 401.)
+    if (context.app == undefined) {
+      throw new functions.https.HttpsError(
+        'failed-precondition',
+        'The function must be called from an App Check verified app.',
+      );
+    }
+
+    // Checking attribute.
+    // if (!(typeof photos === 'object') || photos.length === 0) {
+    //   // Throwing an HttpsError so that the client gets the error details.
+    //   throw new functions.https.HttpsError(
+    //     'invalid-argument',
+    //     'The function must be called with ' + 'one arguments "text" containing the message text to add.',
+    //   );
+    // }
+
+    //  if (!context.auth) return { status: 'error', code: 401, message: 'Not signed in' };
+
+    // // Checking that the user is authenticated.
+    // if (!context.auth) {
+    //   // Throwing an HttpsError so that the client gets the error details.
+    //   throw new functions.https.HttpsError(
+    //     'failed-precondition',
+    //     'The function must be called ' + 'while authenticated.',
+    //   );
+    // }
+
+    const client = new vision.ImageAnnotatorClient({
+      keyFilename: '../credentials/google_credentials.json',
+    });
+
+    // const localFile =
+    //   '/Users/ninja/Projects/4tune/functions/__tests__/images/4VfLNhjOjwXwvLEK2qE6t3W9Hgb2-1664243751757.jpeg';
+
+    try {
+      // const [result] = await client.textDetection(`${localFile}`);
+      // const [result] = await client.textDetection(`gs://tune-363401.appspot.com/tickets/${fileName}.jpg`);
+      const [result] = await client.textDetection(
+        `gs://tune-363401.appspot.com/tickets/4VfLNhjOjwXwvLEK2qE6t3W9Hgb2/4VfLNhjOjwXwvLEK2qE6t3W9Hgb2-1664245175065.jpg`,
+      );
+      console.log('🚀 ~ file: index.ts ~ line 52 ~ .onCall ~ result', result);
+
+      const detections = result.textAnnotations;
+      // detections?.forEach((text: string) => functions.logger.info(text));
+
+      return detections;
+      // TODO: fix any
+    } catch (error: any) {
+      functions.logger.error(error);
+      throw new functions.https.HttpsError('failed-precondition', { ...error });
+    }
   });
-
-  // TODO: this is for local path, haven't been able to make it work
-  const fileName = '/Users/ninja/Downloads/sample.jpeg';
-
-  try {
-    const [result] = await client.textDetection(fileName);
-    // const [result] = await client.textDetection(`gs://tune-363401.appspot.com/${fileName}`);
-
-    const detections = result.textAnnotations;
-    functions.logger.info('Text:');
-    detections?.forEach((text: string) => functions.logger.info(text));
-    response.status(200).send('Hello from Firebase!');
-  } catch (error) {
-    functions.logger.error(error);
-    response.status(500).send(error);
-  }
-});
