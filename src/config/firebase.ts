828ad5c21ec69a36740906eeac6a8e0fbaab353b
Mike Doubintchik <mike.doubintchik@gmail.com>
2022-09-27 09:31:56 -0400

    updated firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,6 +7,13 @@ import { getStorage } from 'firebase/storage';
 import config from './config';
 const { initializeAppCheck, ReCaptchaV3Provider } = require('firebase/app-check');
 
+if (process.env.REACT_APP_ENV === 'development') {
+  console.log('development environment');
+
+  // use debug token for app check
+  (window as any).FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+}
+
 const app = initializeApp(config.firebase);
 
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
@@ -26,10 +33,7 @@ export const db = getFirestore(app);
 export default app;
 
 if (process.env.REACT_APP_ENV === 'development') {
-  console.log('development environment');
-
   // use debug token for app check
-  (window as any).FIREBASE_APPCHECK_DEBUG_TOKEN = true;
 
   connectFunctionsEmulator(functions, 'localhost', 5001);
 }
