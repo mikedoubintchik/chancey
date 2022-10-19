f91ff0557a54c70d9db759acfc39b27a7317763c
Moshe Moryosef <moshemoryosef@Moshes-MBP.attlocal.net>
2022-10-19 19:30:29 -0400

    start stop generating

--- src/components/drawings-generator/DrawingsGenerator.tsx
+++ src/components/drawings-generator/DrawingsGenerator.tsx
@@ -1,5 +1,6 @@
-import { IonCard, IonCardContent, IonItem } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonIcon, IonItem } from '@ionic/react';
 import Series from 'components/series/Series';
+import { pauseOutline, playOutline } from 'ionicons/icons';
 import { useCallback, useEffect, useState } from 'react';
 import { RuleEngineClient } from 'rules/RuleEngineClient';
 import { SeriesModel } from 'types/series';
@@ -12,10 +13,11 @@ interface DrawingsGeneratorProps {
 
 const DrawingsGenerator: React.FC<DrawingsGeneratorProps> = ({ count, showMax }) => {
   const [drawings, setDrawings] = useState<Array<SeriesModel>>([]);
+  const [generating, setGenerating] = useState<boolean>(true);
 
   const generateDrawings = useCallback(async () => {
     await new Promise((r) => setTimeout(r, 5000));
-    if (RuleEngineClient.instance.isInitialized) {
+    if (RuleEngineClient.instance.isInitialized && generating) {
       let drawingResponse = await RuleEngineClient.instance.generateDrawings(count);
       let newDrawingsState = drawings.concat(drawingResponse.drawings);
       // drawingResponse.drawings.forEach((drawing) => {
@@ -29,12 +31,15 @@ const DrawingsGenerator: React.FC<DrawingsGeneratorProps> = ({ count, showMax })
       setDrawings(newDrawingsState);
     }
     // }
-  }, [drawings]);
+  }, [drawings, generating]);
 
   useEffect(() => {
     generateDrawings();
   }, [generateDrawings]);
 
+  const togglePlayMode = () => {
+    setGenerating(!generating);
+  };
   const renderDrawings = () => {
     return [...drawings].reverse().map((drawing) => {
       return <Series key={drawing.numbers.join('-')} numbers={drawing.numbers} extra={drawing.extra} />;
@@ -42,6 +47,13 @@ const DrawingsGenerator: React.FC<DrawingsGeneratorProps> = ({ count, showMax })
   };
   return (
     <IonCard>
+      <IonCardHeader>
+        <IonItem lines="none">
+          <IonButton onClick={() => togglePlayMode()} fill="clear" shape="round" slot="start">
+            <IonIcon icon={generating ? pauseOutline : playOutline} />
+          </IonButton>
+        </IonItem>
+      </IonCardHeader>
       <IonCardContent>{renderDrawings()}</IonCardContent>
     </IonCard>
   );
