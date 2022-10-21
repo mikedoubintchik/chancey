5aca82775585b5d7e6ced887f7ac6effab1aefd3
Moshe Moryosef <moshemoryosef@Moshes-MBP.attlocal.net>
2022-10-20 21:08:43 -0400

    better approach in drawings generator

--- src/components/drawings-generator/DrawingsGenerator.tsx
+++ src/components/drawings-generator/DrawingsGenerator.tsx
@@ -1,8 +1,9 @@
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonIcon, IonItem } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonIcon, IonItem, IonLabel } from '@ionic/react';
 import Series from 'components/series/Series';
 import { pauseOutline, playOutline } from 'ionicons/icons';
 import { useCallback, useEffect, useState } from 'react';
 import { RuleEngineClient } from 'rules/RuleEngineClient';
+import { useStore } from 'stores/store';
 import { SeriesModel } from 'types/series';
 import './DrawingsGenerator.css';
 
@@ -12,26 +13,32 @@ interface DrawingsGeneratorProps {
 }
 
 const DrawingsGenerator: React.FC<DrawingsGeneratorProps> = ({ count, showMax }) => {
+  const { state } = useStore();
   const [drawings, setDrawings] = useState<Array<SeriesModel>>([]);
   const [generating, setGenerating] = useState<boolean>(true);
+  const [timeLeft, setTimeLeft] = useState<number>(5);
 
   const generateDrawings = useCallback(async () => {
-    await new Promise((r) => setTimeout(r, 5000));
-    if (RuleEngineClient.instance.isInitialized && generating) {
-      let drawingResponse = await RuleEngineClient.instance.generateDrawings(count);
-      let newDrawingsState = drawings.concat(drawingResponse.drawings);
-      // drawingResponse.drawings.forEach((drawing) => {
-      //   newDrawingsState.unshift(drawing);
-      // });
-      //.concat(drawingResponse.drawings);
-      if (newDrawingsState.length > showMax) {
-        newDrawingsState.splice(0, 1);
+    if (timeLeft > 0) {
+      await new Promise((r) => setTimeout(r, 1000));
+      setTimeLeft(timeLeft - 1);
+    } else {
+      if (RuleEngineClient.instance.isInitialized && generating) {
+        let drawingResponse = await RuleEngineClient.instance.generateDrawings(count);
+        let newDrawingsState = drawings.concat(drawingResponse.drawings);
+
+        if (newDrawingsState.length > showMax) {
+          newDrawingsState.splice(0, 1);
+        }
+        // console.log(
+        //   '🚀 ~ file: DrawingsGenerator.tsx ~ line 20 ~ generateADrawing ~ newDrawingsState',
+        //   newDrawingsState,
+        // );
+        setDrawings(newDrawingsState);
       }
-      console.log('🚀 ~ file: DrawingsGenerator.tsx ~ line 20 ~ generateADrawing ~ newDrawingsState', newDrawingsState);
-      setDrawings(newDrawingsState);
+      setTimeLeft(5);
     }
-    // }
-  }, [drawings, generating]);
+  }, [drawings, generating, state, timeLeft]);
 
   useEffect(() => {
     generateDrawings();
@@ -40,6 +47,7 @@ const DrawingsGenerator: React.FC<DrawingsGeneratorProps> = ({ count, showMax })
   const togglePlayMode = () => {
     setGenerating(!generating);
   };
+
   const renderDrawings = () => {
     return [...drawings].reverse().map((drawing, index) => {
       return (
@@ -56,6 +64,8 @@ const DrawingsGenerator: React.FC<DrawingsGeneratorProps> = ({ count, showMax })
           <IonButton onClick={() => togglePlayMode()} fill="clear" shape="round" slot="start">
             <IonIcon icon={generating ? pauseOutline : playOutline} />
           </IonButton>
+          {generating && <IonLabel color="primary">Generating next drawing in {timeLeft}s...</IonLabel>}
+          {!generating && <IonLabel color="danger">Paused</IonLabel>}
         </IonItem>
       </IonCardHeader>
       <IonCardContent>{renderDrawings()}</IonCardContent>
