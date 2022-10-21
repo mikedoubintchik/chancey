873698d04ab9405cbda2f1e8f3f9caf5164181c7
Moshe Moryosef <moshemoryosef@Moshes-MBP.attlocal.net>
2022-10-21 16:15:24 -0400

    soem cleanups

--- src/components/rules-engine/add-rule-modal/AddableRule.tsx
+++ src/components/rules-engine/add-rule-modal/AddableRule.tsx
@@ -1,18 +1,5 @@
-import {
-  IonButton,
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonContent,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonPopover,
-  IonRippleEffect,
-} from '@ionic/react';
-import usePopover from 'hooks/usePopover';
+import { IonButton, IonCard, IonIcon, IonItem, IonLabel, IonRippleEffect } from '@ionic/react';
 import { informationCircleOutline } from 'ionicons/icons';
-import { useRef } from 'react';
 
 import { IRuleBase } from 'rules/RuleBase';
 import { formatPercentage } from 'utils/lottery-utils';
@@ -23,48 +10,31 @@ interface IRuleProps {
   onRuleInformationRequested(rule: IRuleBase): void;
 }
 const AddableRule: React.FC<IRuleProps> = ({ rule, onAddRuleRequested, onRuleInformationRequested }) => {
-  const popover = useRef<HTMLIonPopoverElement>(null);
-  const { isOpen, showPopover, hidePopover } = usePopover();
-
-  const openPopover = (e: any) => {
-    popover.current!.event = e;
-    showPopover();
-  };
-
   return (
-    <IonCard>
-      <IonCardHeader>
-        <IonItem lines="none">
-          <IonLabel color="primary">{rule.name}</IonLabel>
-          <IonIcon onClick={() => onRuleInformationRequested(rule)} icon={informationCircleOutline} slot="end" />
-        </IonItem>
-      </IonCardHeader>
-      <IonCardContent>
-        <IonItem lines="none">{rule.description}</IonItem>
-        <IonItem lines="none">
-          <IonLabel color="medium" class="ion-text-wrap">
-            This rule applied to {formatPercentage(rule.calculatePercentageForRecentDrawings(300))}% of recent 300
-            drawings
-          </IonLabel>
-        </IonItem>
-        <IonItem>
-          <IonButton
-            slot="end"
-            shape="round"
-            // expand="full"
-            onClick={async () => {
-              //loadindicator
-              onAddRuleRequested(rule);
-            }}
-          >
-            Add Rule
-            <IonRippleEffect></IonRippleEffect>
-          </IonButton>
-        </IonItem>
-      </IonCardContent>
-      <IonPopover ref={popover} isOpen={isOpen} onDidDismiss={hidePopover}>
-        <IonContent class="ion-padding">{rule.information}</IonContent>
-      </IonPopover>
+    <IonCard onClick={() => onRuleInformationRequested(rule)}>
+      <IonItem lines="none">
+        <IonLabel color="primary">{rule.name}</IonLabel>
+        <IonIcon onClick={() => onRuleInformationRequested(rule)} icon={informationCircleOutline} slot="end" />
+      </IonItem>
+      <IonItem lines="none">{rule.description}</IonItem>
+      <IonItem lines="none">
+        <IonLabel color="medium" class="ion-text-wrap">
+          This rule applied to {formatPercentage(rule.calculatePercentageForRecentDrawings(300))}% of recent 300
+          drawings
+        </IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonButton
+          slot="end"
+          shape="round"
+          onClick={async () => {
+            onAddRuleRequested(rule);
+          }}
+        >
+          Add Rule
+          <IonRippleEffect></IonRippleEffect>
+        </IonButton>
+      </IonItem>
     </IonCard>
   );
 };
