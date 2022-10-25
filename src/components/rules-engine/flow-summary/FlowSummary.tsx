31f05838f150bcb8537d0e6201e312a1184445df
Moshe Moryosef <moshemoryosef@Moshes-MBP.attlocal.net>
2022-10-25 15:10:49 -0400

    remove some custome styling

--- src/components/rules-engine/flow-summary/FlowSummary.tsx
+++ src/components/rules-engine/flow-summary/FlowSummary.tsx
@@ -1,4 +1,6 @@
-import { IonCard, IonItem } from '@ionic/react';
+import { IonCard, IonCardContent, IonChip, IonIcon, IonItem, IonLabel } from '@ionic/react';
+import { arrowUp } from 'ionicons/icons';
+import { IRuleBase } from 'rules/RuleBase';
 import { useStore } from 'stores/store';
 import { formatPercentage } from 'utils/lottery-utils';
 import './FlowSummary.css';
@@ -8,15 +10,26 @@ interface IFlowSummaryProps {
 }
 const FlowSummary: React.FC<IFlowSummaryProps> = ({ postRuleCombsCount }) => {
   const { state } = useStore();
+
+  const getChancesView = () => (
+    <IonChip color="success" style={{ margin: '0 auto' }}>
+      Your chances are improved by {formatPercentage(1 - postRuleCombsCount / state.initialChances)}%
+    </IonChip>
+  );
   return (
     // <IonCard>
     <IonCard class="rule-item">
-      <IonItem>Your chances to win the lottery are 1:{postRuleCombsCount}!</IonItem>
-      <IonItem>
-        {postRuleCombsCount > 0 &&
-          `This improves your chances by ${formatPercentage(1 - postRuleCombsCount / state.initialChances)}%`}
-        {postRuleCombsCount < 1 && `Please remove some rules...`}
-      </IonItem>
+      <IonCardContent>
+        <IonItem lines="none">
+          <IonLabel style={{ whiteSpace: 'unset' }}>
+            Your chances to win the lottery are 1:{postRuleCombsCount}!
+          </IonLabel>
+        </IonItem>
+        <IonItem lines="none">
+          {postRuleCombsCount > 0 && getChancesView()}
+          {postRuleCombsCount < 1 && `Please remove some rules...`}
+        </IonItem>
+      </IonCardContent>
     </IonCard>
 
     // </IonCard>
