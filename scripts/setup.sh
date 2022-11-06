87cca0b68fffd33bc836e283c28218682a916f20
Mike Doubintchik <mike.doubintchik@gmail.com>
2022-11-06 00:49:58 -0500

    updated code to support releases to play and apple stores

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,9 @@ npm install -g firebase-tools
 curl -s "https://get.sdkman.io" | bash
 sdk install java 11.0.2-open
 curl -sSL https://get.rvm.io | bash -s stable --ruby
-source /Users/ninja/.rvm/scripts/rvm
+source ~/.rvm/scripts/rvm
 gem install bundler
 bundle update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 nvm install 16
+MATCH_PASSWORD=chancey bundle exec fastlane match development --readonly && bundle exec fastlane match appstore
