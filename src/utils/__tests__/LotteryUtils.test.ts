a935123ff9fbee05c501875e5134275ca059c347
Mike Doubintchik <mike.doubintchik@gmail.com>
2023-06-15 23:47:13 -0400

    fixed unit tests for lottery utils

--- src/utils/__tests__/LotteryUtils.test.ts
+++ src/utils/__tests__/LotteryUtils.test.ts
@@ -31,13 +31,13 @@ describe('Lottery Utils', () => {
     const testCases = [
       {
         text: 'Lottery.com NCLottery.com NCLotter\nNorth Carolina\nMEGA\nMILLIONS\nMEGAPLIER\nMB\nA. 02 13 27 42 47 QP 02 QP\nSINGLE DRAW FRI JUL29 22\n1169100\n01VVT3&M\nJUL27 22/09:59\n$3.00\nLucke-Rewards Entry Code\nBE\n02NGD - JG6FY - 92200 - H03CV - 9QYTT',
-        ticketDate: 'JUL27 22',
-        drawingDate: 'JUL29 22',
+        ticketDate: 'July 27, 2022',
+        drawingDate: 'July 29, 2022',
       },
       {
         text: 'Lottery.com NCLottery.com NCLotter\nNorth Carolina\nMEGA\nMILLIONS\nMEGAPLIER\nMB\nA. 02 13 27 42 47 QP 02 QP\nSINGLE DRAW FRI JUL 29, 2022\n1169100\n01VVT3&M\nJUL 27, 2022 22:09:59\n$3.00\nLucke-Rewards Entry Code\nBE\n02NGD - JG6FY - 92200 - H03CV - 9QYTT',
-        ticketDate: 'JUL 27, 2022',
-        drawingDate: 'JUL 29, 2022',
+        ticketDate: 'July 27, 2022',
+        drawingDate: 'July 29, 2022',
       },
       {
         text: 'No dates in this string',
